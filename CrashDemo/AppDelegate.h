9be9312eb8fc89b97885adc2361d2b14698f1366
Lix <xiao.liunit@gmail.com>
2017-02-07 13:47:20 +0800

    Initial Commit

---
+++ CrashDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrashDemo
+//
+//  Created by Lix on 17/2/7.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrashDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrashDemo
+//
+//  Created by Lix on 17/2/7.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
